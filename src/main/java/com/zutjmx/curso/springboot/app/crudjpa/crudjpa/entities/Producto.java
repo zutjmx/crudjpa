6423a69d73302bec13ce830378b425048d356f60
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-10-08 12:47:23 -0600

    Add Spring Boot Actuator dependency, create Producto entity, and update application properties for database configuration

---
+++ src/main/java/com/zutjmx/curso/springboot/app/crudjpa/crudjpa/entities/Producto.java
@@ -0,0 +1,51 @@
+package com.zutjmx.curso.springboot.app.crudjpa.crudjpa.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Double precio;
+    private String descripcion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
