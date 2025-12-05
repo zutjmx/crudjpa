a44675e3c720e085bac45f5d456b5924ecfd5799
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-12-05 11:19:16 -0600

    Add AppConfig class for message source configuration and remove redundant PropertySource from CrudjpaApplication

---
+++ src/main/java/com/zutjmx/curso/springboot/app/crudjpa/crudjpa/AppConfig.java
@@ -0,0 +1,10 @@
+package com.zutjmx.curso.springboot.app.crudjpa.crudjpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:messages.properties")
+public class AppConfig {
+
+}
--- src/main/java/com/zutjmx/curso/springboot/app/crudjpa/crudjpa/CrudjpaApplication.java
+++ src/main/java/com/zutjmx/curso/springboot/app/crudjpa/crudjpa/CrudjpaApplication.java
@@ -2,10 +2,8 @@ package com.zutjmx.curso.springboot.app.crudjpa.crudjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:messages.properties")
 public class CrudjpaApplication {
 
 	public static void main(String[] args) {
