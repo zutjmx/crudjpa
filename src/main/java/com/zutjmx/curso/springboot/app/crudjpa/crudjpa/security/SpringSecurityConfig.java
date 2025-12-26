a27a378448cb3e8f9c0cabd2e0057137c23ccb86
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-12-26 13:25:40 -0600

    Add Spring Security configuration with password encoding and stateless session management

--- src/main/java/com/zutjmx/curso/springboot/app/crudjpa/crudjpa/security/SpringSecurityConfig.java
+++ src/main/java/com/zutjmx/curso/springboot/app/crudjpa/crudjpa/security/SpringSecurityConfig.java
@@ -2,8 +2,11 @@ package com.zutjmx.curso.springboot.app.crudjpa.crudjpa.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -11,6 +14,20 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();        
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {        
+        return http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/usuarios").permitAll()
+                .anyRequest().authenticated()
+            )
+            .csrf(config -> config.disable())
+            .sessionManagement(
+                management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .build();
     }
 
 }
